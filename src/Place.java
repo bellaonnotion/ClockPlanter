083da612a86c4261981b20935dfbece9974467f9
Isa <bellaonnotion@gmail.com>
2025-03-03 11:56:33 -0500

    added table

--- src/Place.java
+++ src/Place.java
@@ -1,19 +1,23 @@
 package src;
 
-
-
-
 public class Place {
+
+    String string0 = "1000000";
+    String string1 = "1111001";
+    String string2 = "0100100";
+    String string3 = "0110000";
+    String string4 = "0011001";
+    String string5 = "0010010";
+    String string6 = "0000010";
+    String string7 = "1111000";
+    String string8 = "0000000";
+    String string9 = "0010000";
+
     
     //int[] arr = {}
 
     public void Draw0() {
-        boolean a = true;
-        boolean b = true;
-        boolean c = true;
-        boolean d = true;
-        boolean e = true;
-        boolean f = true;
+        
     }
     public void Draw1() {
         boolean b = true;
